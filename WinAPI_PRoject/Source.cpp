793fd33bb804eb2a6864a82a43f8085fa5382e5a
mosheadd <rost1234567890qgp@gmail.com>
2022-11-19 17:03:26 +0300

    Create MainWindow

---
+++ WinAPI_PRoject/Source.cpp
@@ -0,0 +1,50 @@
+#include <Windows.h>
+
+
+WNDCLASS CreateWindowClass(HBRUSH color, HCURSOR cursor, HINSTANCE hInst, HICON icon, LPCWSTR name, WNDPROC procedure)
+{
+
+	WNDCLASS wndclass = { 0 };
+
+	wndclass.hCursor = cursor;
+	wndclass.hIcon = icon;
+	wndclass.lpszClassName = name;
+	wndclass.hInstance = hInst;
+	wndclass.hbrBackground = color;
+	wndclass.lpfnWndProc = procedure;
+
+	return wndclass;
+}
+
+LRESULT CALLBACK MainClassProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	switch (msg)
+	{
+	case WM_CREATE:
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default: return DefWindowProc(hwnd, msg, wparam, lparam);
+	}
+}
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow)
+{
+
+	WNDCLASS MainClass = CreateWindowClass((HBRUSH)COLOR_WINDOW, LoadCursor(NULL, IDC_ARROW), hInst, LoadIcon(NULL, IDI_QUESTION), L"MainWindow", MainClassProcedure);
+
+	if (!RegisterClass(&MainClass)) return -1;
+
+	MSG MainMessage = { 0 };
+
+	CreateWindow(MainClass.lpszClassName, L"Okno", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 300, 300, 500, 500, NULL, NULL, NULL, NULL);
+
+	while (GetMessage(&MainMessage, NULL, NULL, NULL))
+	{
+		TranslateMessage(&MainMessage);
+		DispatchMessageW(&MainMessage);
+	}
+
+	return 0;
+}
