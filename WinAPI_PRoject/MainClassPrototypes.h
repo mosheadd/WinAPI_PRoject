df533e72d64efe62bb46ee87950201b27405ff25
mosheadd <rost1234567890qgp@gmail.com>
2022-11-20 17:06:24 +0300

    Create menu and exit option

--- WinAPI_PRoject/MainClassPrototypes.h
+++ WinAPI_PRoject/MainClassPrototypes.h
@@ -1,7 +1,14 @@
 #pragma once
 #include <Windows.h>
 
+#define MenuExit 0
+#define OnMenuCLicked 1
+#define YES IDYES
+#define NO IDNO
+
 
 LRESULT CALLBACK MainClassProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 
 WNDCLASS CreateWindowClass(HBRUSH color, HCURSOR cursor, HINSTANCE hInst, HICON icon, LPCWSTR name, WNDPROC procedure);
+
+void AddMainWindowMenu(HWND hwnd);
--- WinAPI_PRoject/Source.cpp
+++ WinAPI_PRoject/Source.cpp
@@ -41,7 +41,23 @@ LRESULT CALLBACK MainClassProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM l
 {
 	switch (msg)
 	{
+	case WM_COMMAND: 
+		switch (wparam)
+		{
+		case MenuExit:
+			int result = MessageBox(hwnd, L"�� ������������� ������ �����?", L"����� �� ����������", MB_YESNO);
+			switch (result)
+			{
+			case YES:
+				PostQuitMessage(0);
+			case NO:
+				break;
+			}
+			break;
+		}
+		break;
 	case WM_CREATE:
+		AddMainWindowMenu(hwnd);
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
@@ -49,6 +65,17 @@ LRESULT CALLBACK MainClassProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM l
 	default: return DefWindowProc(hwnd, msg, wparam, lparam);
 	}
 }
+
+void AddMainWindowMenu(HWND hwnd)
+{
+	HMENU root_menu = CreateMenu();
+	HMENU exit_app = CreateMenu();
+
+	AppendMenuW(exit_app, MF_STRING, MenuExit, L"�����");
+	AppendMenuW(root_menu, MF_POPUP, (UINT_PTR)exit_app, L"����������");
+
+	SetMenu(hwnd, root_menu);
+} 
 
 {
 
