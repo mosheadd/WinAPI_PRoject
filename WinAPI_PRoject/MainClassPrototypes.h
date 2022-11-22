a44a0208e6a72a1cad6bb59d01dbbc3df6648b42
mosheadd <rost1234567890qgp@gmail.com>
2022-11-22 18:32:51 +0300

    Add comments

--- WinAPI_PRoject/MainClassPrototypes.h
+++ WinAPI_PRoject/MainClassPrototypes.h
@@ -1,13 +1,17 @@
 #pragma once
 #include <Windows.h>
 
+//Все определения
 #define MenuExit 1
 #define OnMenuCLicked 1
 #define OnMouseClicked 2
 #define YES IDYES
 #define NO IDNO
 
+//Виджеты
 
+
+//Прототипы функций для главного окна
 LRESULT CALLBACK MainClassProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 
 WNDCLASS CreateWindowClass(HBRUSH color, HCURSOR cursor, HINSTANCE hInst, HICON icon, LPCWSTR name, WNDPROC procedure);
